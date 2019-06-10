323afc930b1e68182639bd8c73460ba95a2d69a6
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2019-06-09 21:59:49 -0300

    Add files via upload

--- lista2seminarios/q06_Diogo_649651.java
+++ lista2seminarios/q06_Diogo_649651.java
@@ -6,7 +6,8 @@
 //
 
 import java.util.Scanner;
-
+import java.util.Arrays;
+import java.util.Collections;
 // O programa deve contar quantos pares de botas de tamanhos iguais para os dois pés são possíveis de serem formados.
 // Para tal, usarei classes. 
 // A entrada será ordenada, e serão criados objetos para cada tamanho de bota. Cada par E/D incrementará em 1 o contador do objeto.
@@ -29,12 +30,33 @@ public class q06_Diogo_649651{
     for(int i = 0; i < numEntradas; i++){
     
         conjuntoBotas[i] = input.nextLine();
-        conjuntoBotas[i] = conjuntoBotas[i].trim();
 
     }
 
-    //Ordenar as entradas numericamente e contar quantas ocorrencias unicas de cada tipo;
+    Arrays.sort(conjuntoBotas);
 
+    //arrays pra armazenar tams e pes
+
+    String[] conjuntoTamanhos = new String[numEntradas];
+    String[] conjuntoPes = new String[numEntradas];
+
+    //Criar um array splitado com os tamanhos e, em outro array, no mesmo índice, o pé.
+
+    String[] array = new String[2];
+   
+    for(int i = 0; i<numEntradas;i++){
+  
+    array = conjuntoBotas[i].split("\\s+");
+
+    conjuntoTamanhos[i] = array[0];
+    conjuntoPes[i] = array[1];
+
+
+   } 
+
+   for(int j = 0;j<numEntradas;j++){
+       System.out.println(conjuntoBotas[j]);
+   }
     //Criar os objetos 
     //Chamar os metodos
     //Somar os pares
@@ -45,42 +67,5 @@ public class q06_Diogo_649651{
 
 
 
-
-}
-
-class bota{
-private int pesEsquerdos = 0;
-private int pesDireitos = 0;
-
-private int NumPares = 0;
-
-public static void addEsquerdo(){
-
-this.pesEsquerdos++;
-
-}
-public static void addDireito(){
-
-this.pesDireitos++;
-
-}
-public static int paresFormados(){
-    int numDireitos = this.pesDireitos;
-    int numEsquerdos = this.pesEsquerdos;
-
-      if(numDireitos > 0 && numEsquerdos > 0){
-
-            int menor = numEsquerdos;
-
-            if(numDireitos < menor)
-                menor = numDireitos;
-
-
-            return menor;
-
-        }
-      else
-        return 0;  
-}
 
 }
